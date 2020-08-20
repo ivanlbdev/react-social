2060f2269efe3c3ea43ebcc9fb145e2df79e011b
ivanlbdev <jean.cygne@me.com>
2020-08-20 22:55:39 +0300

    git

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import c from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
+
+const DialogItem = (props) => {
+	return (
+		<div className={c.item + ' ' + c.active}>
+			<NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+		</div>
+	)
+}
+
+const Message = (props) => {
+	return (
+		<div className={c.message}>{props.message}</div>
+	)
+}
 
 const Dialogs = () => {
 	return (
 		<div className={c.dialogs}>
 			<div className={c.dialogs_items}>
-				<div className={c.item + ' ' + c.active}>
-					Сонечка
-				</div>
-				<div className={c.item}>
-					Ванечка
-				</div>
-				<div className={c.item}>
-					Макчимочка
-				</div>
-				<div className={c.item}>
-					Санечка
-				</div>
-				<div className={c.item}>
-					Денисочка
-				</div>
+				<DialogItem name='Сонечка' id='1' />
+				<DialogItem name='Ванечка' id='2' />
+				<DialogItem name='Санечка' id='3' />
+				<DialogItem name='Максимочка' id='4' />
+				<DialogItem name='Денисочка' id='5' />
 			</div>
 			<div className={c.messages}>
-				<div className={c.message}>Привет</div>
-				<div className={c.message}>Как дела</div>
-				<div className={c.message}>Что делаешь</div>
-				<div className={c.message}>Да, все неплохо</div>
+				<Message message="Hello" />
+				<Message message="you" />
+				<Message message="Как дела?" />
 			</div>
 		</div>
 	)
