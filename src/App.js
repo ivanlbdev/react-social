c156cddaff1a5b07fe5822dc840644be7d7876f7
ivanlbdev <jean.cygne@me.com>
2020-09-12 15:48:25 +0300

    поменяли компонент на рендер

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ const App = () => {
           <div className="main-content">
             <Navigation />
             <div className="app_wrapper_content">
-              <Route path='/profile' component={Profile} />
-              <Route path='/dialogs' component={Dialogs} />
-              <Route path='/news' component={News} />
-              <Route path='/music' component={Musics} />
-              <Route path='/settings' component={Settings} />
+              <Route path='/profile' render={() => <Profile />} />
+              <Route path='/dialogs' render={() => <Dialogs />} />
+              <Route path='/news' render={() => <News />} />
+              <Route path='/music' render={() => <Musics />} />
+              <Route path='/settings' render={() => <Settings />} />
             </div>
           </div>
         </div>
