43b14ab3ea02adad48476844e4300849d123122f
ivanlbdev <jean.cygne@me.com>
2020-09-13 10:24:29 +0300

    рефки добавили в посты на текстареа

--- src/components/profile/MyPosts/MyPosts.jsx
+++ src/components/profile/MyPosts/MyPosts.jsx
@@ -6,6 +6,14 @@ const MyPosts = (props) => {
 
 
 	let postElements = props.postsData.map((post) => <Post message={post.message} likeNumber={post.likesCount} />)
+
+	let newPostElement = React.createRef();
+
+	let addPost = () => {
+		let text = newPostElement.current.value;
+		alert(text);
+	}
+
 	return (
 		<div>
 			<div className={c.posts}>
@@ -13,8 +21,8 @@ const MyPosts = (props) => {
 					<h2>Что у вас нового?</h2>
 				</div>
 				<div className={c.get_posts}>
-					<textarea name="" id="" cols="90" rows="4"></textarea>
-					<button>Опубликовать</button>
+					<textarea ref={newPostElement} cols="90" rows="4"></textarea>
+					<button onClick={addPost}>Опубликовать</button>
 				</div>
 			</div>
 			{postElements}
